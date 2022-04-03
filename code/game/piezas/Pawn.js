12d9203301f3731edd2f1f7b74d0aaaea22b6474
alikates <alikates@dnyon.com>
2022-04-03 19:09:09 +0200

    Add all possible pawn moves

--- code/game/piezas/Pawn.js
+++ code/game/piezas/Pawn.js
@@ -1,24 +1,34 @@
-import Piece from "./Piece";
+import Piece, { WhitePlayer } from "./Piece";
 
 class Pawn extends Piece {
-  constructor(board, xPos = 1, yPos = 1) {
-    super(board, xPos, yPos);
-  }
+	constructor(player, xPos = 1, yPos = 1) {
+		super(player, xPos, yPos);
+	}
 
-  getAllowedMoves() {
-    const possibleMoves = [{ x: 0, y: 1 }];
-    if (this.pos.x === 1) {
-      possibleMoves.push({ x: 0, y: 2 });
-    }
+	getAllowedMoves() {
+		const possibleMoves = [];
+		const allowedMoves = [];
 
-    const allowedMoves = [];
+		if (this.player === WhitePlayer) {
+			possibleMoves.push({ x: 0, y: 1 });
 
-    possibleMoves.forEach((elem) => {
-      if (this.checkMoveRange(elem.x, elem.y)) allowedMoves.push(elem);
-    });
+			if (this.pos.y === 1) {
+				possibleMoves.push({ x: 0, y: 2 });
+			}
+		} else {
+			possibleMoves.push({ x: 0, y: -1 });
 
-    return allowedMoves;
-  }
+			if (this.pos.y === 6) {
+				possibleMoves.push({ x: 0, y: -2 });
+			}
+		}
+
+		possibleMoves.forEach((elem) => {
+			if (this.checkMoveRange(elem.x, elem.y)) allowedMoves.push(elem);
+		});
+
+		return allowedMoves;
+	}
 }
 
 export default Pawn;
