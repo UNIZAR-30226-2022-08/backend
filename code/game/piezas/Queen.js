6052db219a0a5c8cb284c9391d02f80cc4e060d1
alikates <alikates@dnyon.com>
2022-04-07 22:41:36 +0200

    Update pieces

--- code/game/piezas/Queen.js
+++ code/game/piezas/Queen.js
@@ -1,8 +1,33 @@
-import Piece from "./Piece";
+import Piece, { BlackPlayer } from "./Piece";
 
 class Queen extends Piece {
-	constructor(player, xPos = 1, yPos = 1) {
-		super(player, xPos, yPos);
+	constructor(player, game, xPos = 1, yPos = 1) {
+		super(player, game, xPos, yPos);
+	}
+
+	#walk(x, y, xMov, yMov) {
+		const newX = x + xMov;
+		const newY = y + yMov;
+
+		let arr = [];
+		if (this.checkMoveRange(this.pos.x + newX, this.pos.y + newY)) {
+			arr.push({ x: newX, y: newY });
+			if (!this.game.getPiece(!this.player, newX, newY)) {
+				arr = arr.concat(this.#walk(newX, newY, xMov, yMov));
+			}
+		}
+		return arr;
+	}
+
+	getAllowedMoves() {
+		return this.#walk(0, 0, 1, 1)
+			.concat(this.#walk(0, 0, -1, -1))
+			.concat(this.#walk(0, 0, 1, -1))
+			.concat(this.#walk(0, 0, -1, 1))
+			.concat(this.#walk(0, 0, 1, 0))
+			.concat(this.#walk(0, 0, -1, 0))
+			.concat(this.#walk(0, 0, 0, 1))
+			.concat(this.#walk(0, 0, 0, -1));
 	}
 }
 
