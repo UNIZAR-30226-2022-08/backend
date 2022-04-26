05a1954bb3df4e6444f6d55bb1665ccbd297a455
alikates <alikates@dnyon.com>
2022-04-26 20:44:43 +0200

    move code from routes to the UserController file

--- code/routes/account.js
+++ code/routes/account.js
@@ -1,7 +1,7 @@
-import { Router } from 'express';
-import User from '../models/user';
+import { Router } from "express";
+import UserController from "../controller/UserController";
 
-const router = Router()
+const router = Router();
 
 // middleware that is specific to this router
 /* router.use((req, res, next) => {
@@ -10,71 +10,27 @@ const router = Router()
 })
 */
 
-router.post('/register', async function(req, res) {
-	const {username, email, password } = req.body
-	await User.create({
-		username,
-		email,
-		password
-	}). then(function(){
-		res.status(201).send();
-	})
-	.catch(function(err) {
-		res.status(400).json({ error: err.errors}).send()
-	});
-});
-	
-router.post('/login', async function(req, res) {
-	if (req.session.userId) {
-		res.status(403).json({ status: 'failure', message: 'User already logged in' })
-		return
-	}
-	const {email, password} = req.body;
-	await User.findOne({
-		where: {
-			email,
-			password
-		}
-	}).then(function(user) {
-		if(user){
-			const {session} = req;
-			session.userId=user.dataValues.userId
-			session.username=user.dataValues.username
-			session.email=user.dataValues.email
-			res.status(200).send();
-		} else {
-			res.status(400).json({ error: "Invalid email or password"}).send();
-			
-		}
-	})
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.post("/changePassword", UserController.changePassword);
+router.post("/logout", UserController.logout);
+
+router.all("/checkSession", (req, res) => {
+	res.json(req.session);
 });
 
+// router.all('/getAllUsers', async (req,res) => {
+// 	await User.findAll({
+// 	}).then(function(user) {
+// 		res.status(200).json(user)
+// 	})
+// });
 
-router.post('/changePassword', async function(req, res) {
-	const {email, newPassword} = req.body
-	await User.update({
-		password: newPassword,
-		where: {
-			email
-		}
-	})
-	.then(function() {
-		res.status(201).send();
-	})
-	.catch(function(err) {
-		res.status(400).json({ error: err.errors}.send())
-		
-	});
-	
-});
+// router.all('/login', async function(req, res) {
+// 	await User.findAll({
+// 	}).then(function(user) {
+// 		res.status(200).json(user)
+// 	})
+// });
 
-router.all('/checkSession',(req,res) => {
-		res.json(req.session);
-});
-
-router.post("/logout",(req,res) => {
-		req.session.destroy();
-		res.status(201).json({ status: 'success', message: 'Logged out' })
-});
-
-export default router
+export default router;
