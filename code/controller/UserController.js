5342b80fd2f44d5ac00d40e59bbef37c54b3d5db
alikates <alikates@dnyon.com>
2022-04-28 17:09:31 +0200

    Add password hashing and formatting

--- code/controller/UserController.js
+++ code/controller/UserController.js
@@ -1,113 +1,112 @@
-import { Router } from 'express';
-import User from '../models/User';
+import { Router } from "express";
+import User from "../models/User";
 
 const UserController = {
-    async register(req, res) {
-        const {username, email, password } = req.body
-        await User.create({
-            username,
-            email,
-            password
-        }). then(function(){
-            res.status(201).send();
-        })
-        .catch(function(err) {
-            res.status(400).json({ error: err.errors}).send()
-        });
-    },
-    async login(req, res) {
-        if (req.session.username) {
-            res.status(403).json({ message: 'User already logged in' })
-            return
-        }
-        const {email, password} = req.body;
-        await User.findOne({
-            where: {
-                email: password,
-                password: password
-            }
-        }).then(function(user) {
-            if(user.length !== 0){
-                const {session} = req;
-                session.username=user.dataValues.username
-                session.email=user.dataValues.email
-                res.status(200).send();
-            } else {
-                res.status(400).json({ error: "Invalid email or password"}).send();
-                
-            }
-        })
-    },
-    async changePassword(req, res) {
-        const {email, newPassword} = req.body
-        await User.update({
-            password: newPassword,
-            where: {
-                email
-            }
-        })
-        .then(function() {
-            res.status(201).send();
-        })
-        .catch(function(err) {
-            res.status(400).json({ error: err.errors}.send())
-            
-        });
-    },
-    async logout(req,res) {
-        req.session.destroy();
-        res.status(201).json({ status: 'success', message: 'Logged out' })
-    },
-    async getPublicProfile(req,res) {
-        const {username} = req.body
-        let user = User.findOne({
-            attributes: { include: ["username", "elo", "money"] },
-            where: {
-                username: username
-            }
-        })
-		if (user === null) {
-			res.status(400).json({ error:"Couldn't find the game"})
-			res.send()
-			return
+	async register(req, res) {
+		const { username, email, password } = req.body;
+		await User.create({
+			username,
+			email,
+			password,
+		})
+			.then(function () {
+				res.status(201).send();
+			})
+			.catch(function (err) {
+				res.status(400).json({ error: err.errors }).send();
+			});
+	},
+	async login(req, res) {
+		if (req.session.username) {
+			res.status(403).json({ message: "User already logged in" });
+			return;
 		}
-        else {
-            var { count, rows } = await Project.findAndCountAll({
-                where: {
-                    inProgress: false,
-                    [Op.or]: [{ whitePlayer: username }, { blackPlayer: username }]
-                },
-            });
-            var playedCount = count
+		const { email, password } = req.body;
+		User.findOne({
+			where: {
+				email: password,
+			},
+		})
+			.then(async (user) => {
+				if (user.validPassword(password)) {
+					const { session } = req;
+					session.username = user.dataValues.username;
+					session.email = user.dataValues.email;
+					res.status(200).send();
+				} else {
+					throw { err: "Invalid password" };
+				}
+			})
+			.catch(() => {
+				res.status(400).json({ error: "Invalid email or password" }).send();
+			});
+	},
+	async changePassword(req, res) {
+		const { email, newPassword } = req.body;
+		await User.update({
+			password: newPassword,
+			where: {
+				email,
+			},
+		})
+			.then(function () {
+				res.status(201).send();
+			})
+			.catch(function (err) {
+				res.status(400).json({ error: err.errors }.send());
+			});
+	},
+	async logout(req, res) {
+		req.session.destroy();
+		res.status(201).json({ status: "success", message: "Logged out" });
+	},
+	async getPublicProfile(req, res) {
+		const { username } = req.body;
+		let user = User.findOne({
+			attributes: { include: ["username", "elo", "money"] },
+			where: {
+				username: username,
+			},
+		});
+		if (user === null) {
+			res.status(400).json({ error: "Couldn't find the game" });
+			res.send();
+			return;
+		} else {
+			var { count, rows } = await Project.findAndCountAll({
+				where: {
+					inProgress: false,
+					[Op.or]: [{ whitePlayer: username }, { blackPlayer: username }],
+				},
+			});
+			var playedCount = count;
 
-            const { count, rows } = await Project.findAndCountAll({
-                where: {
-                    inProgress: false,
-                    [Op.or]: [
-                        { [Op.and]: [{whitePlayer: username}, {whiteWon : true}] }, 
-                        { [Op.and]: [{blackPlayer: username}, {whiteWon : false}] }
-                    ]
-                },
-            });
-            var wonCount = count
+			const { count, rows } = await Project.findAndCountAll({
+				where: {
+					inProgress: false,
+					[Op.or]: [
+						{ [Op.and]: [{ whitePlayer: username }, { whiteWon: true }] },
+						{ [Op.and]: [{ blackPlayer: username }, { whiteWon: false }] },
+					],
+				},
+			});
+			var wonCount = count;
 
+			var stats = {
+				playedGames: playedCount,
+				wonGames: wonCount,
+				playedTournaments: 0,
+				wonTournaments: 0,
+			};
 
-            var stats = {
-                playedGames: playedCount,
-                wonGames: wonCount,
-                playedTournaments : 0,
-                wonTournaments : 0
-            }
+			var response = {
+				user: user,
+				stats: stats,
+			};
+			res.status(200).json({ response: user });
+			res.send();
+		}
+	},
+};
 
-            var response = {
-                user : user,
-                stats : stats
-            }
-            res.status(200).json({ response: user })
-			res.send()
-        }
-    }
-}
-
-
-export default UserController
+export default UserController;
--- code/models/User.js
+++ code/models/User.js
@@ -1,62 +1,101 @@
-import { INTEGER, STRING } from 'sequelize';
-import sequelize from '../sequelize/sequelize';
-import AsyncGame from "./AsyncGame"
+import { INTEGER, STRING } from "sequelize";
+import sequelize from "../sequelize/sequelize";
+import bcrypt from "bcrypt";
 
-const User = sequelize.define('user', {
-	username: {
-		type: STRING,
-		unique: true,
-		allowNull: false,
-		primaryKey: true
+const User = sequelize.define(
+	"user",
+	{
+		username: {
+			type: STRING,
+			unique: true,
+			allowNull: false,
+			primaryKey: true,
+		},
+		email: {
+			type: STRING,
+			unique: true,
+			allowNull: false,
+			validate: {
+				isEmail: true,
+			},
+		},
+		password: {
+			type: STRING,
+			allowNull: false,
+			validate: {
+				len: [6, 12],
+			},
+		},
+		elo: {
+			type: INTEGER,
+			defaultValue: 800,
+			validate: {
+				min: 0,
+			},
+		},
+		money: {
+			type: INTEGER,
+			defaultValue: 0,
+			validate: {
+				min: 0,
+			},
+		},
 	},
-	email: {
-		type: STRING,
-		unique: true,
-		allowNull: false,
-		validate: {
-			isEmail: true
-		}
-	},
-	password: {
-		type: STRING,
-		allowNull: false,
-		validate: {
-			len: [6,12]
-		}
-	},
-	elo : {
-		type: INTEGER,
-		defaultValue: 800,
-		validate: {
-			min: 0
-		}
-	},
-	money : {
-		type: INTEGER,
-		defaultValue: 0,
-		validate: {
-			min: 0
-		}
+	{
+		hooks: {
+			beforeCreate: async (user) => {
+				if (user.password) {
+					user.password = bcrypt.hash(user.password, bcrypt.genSalt());
+				}
+			},
+			beforeUpdate: async (user) => {
+				if (user.password) {
+					user.password = bcrypt.hashSync(user.password, bcrypt.genSalt());
+				}
+			},
+		},
+		instanceMethods: {
+			validPassword: (password) => bcrypt.compareSync(password, this.password),
+		},
+		freezeTableName: true, // Model tableName will be the same as the model name
 	}
-}, {
-	freezeTableName: true // Model tableName will be the same as the model name
+);
+
+User.prototype.validPassword = async (password, hash) =>
+	bcrypt.compare(password, hash);
+
+User.hasMany(User, {
+	foreignKey: {
+		allowNull: false,
+		foreignKey: "friends",
+	},
 });
 
-User.hasMany(AsyncGame, { foreignKey: {
-    allowNull: false,
-	foreignKey: 'whitePlayer'
-} })
-AsyncGame.belongsTo(User)
-User.hasMany(AsyncGame, { foreignKey: {
-    allowNull: false,
-	foreignKey: 'blackPlayer'
-} })
-AsyncGame.belongsTo(User)
-
+User.hasMany(User, {
+	foreignKey: {
+		allowNull: false,
+		foreignKey: "friendRequests",
+	},
+});
 
 await User.sync({ alter: true });
-await User.create({ username: "user1", password: "password", email: "a@b.com", elo:800 });
-await User.create({ username: "user2", password: "password", email: "a@c.com", elo:800 });
-await User.create({ username: "user3", password: "password", email: "a@d.com", elo:1000 });
+await User.create({
+	username: "user1",
+	password: "password",
+	email: "a@b.com",
+	elo: 800,
+});
+await User.create({
+	username: "user2",
+	password: "password",
+	email: "a@c.com",
+	elo: 800,
+});
+await User.create({
+	username: "user3",
+	password: "password",
+	email: "a@d.com",
+	elo: 1000,
+});
 
-export default User
+export default User;
