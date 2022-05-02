d7e9fb5c090b4e01443b59f85b8a86a37de2e17d
alikates <alikates@dnyon.com>
2022-05-03 01:01:04 +0200

    Rename sequelize to database

---
+++ code/database/database.js
@@ -0,0 +1,29 @@
+import Sequelize from "sequelize";
+
+const database = new Sequelize(process.env.DB_CONN_URI, {
+	dialect: "postgres",
+	// dialectOptions: {
+	// 	ssl: {
+	// 		require: false,
+	// 		rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+	// 	},
+	// },
+	pool: {
+		max: 9,
+		min: 0,
+		idle: 10000,
+	},
+	logging: process.env.NODE_ENV === "dev" ? false : console.log,
+});
+
+database
+	.authenticate()
+	.then(() => {
+		console.log("Success!");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+database.sync({ alter: true, force: true });
+
+export default database;
