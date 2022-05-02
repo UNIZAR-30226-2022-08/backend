81a73b3e9f0a1a100d6f29f75bb7c0290c630d16
alikates <alikates@dnyon.com>
2022-05-03 00:59:28 +0200

    Formatting and added functions to manage friends

--- code/routes/account.js
+++ code/routes/account.js
@@ -3,6 +3,9 @@ import UserController from "../controller/UserController";
 
 const router = Router();
 
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+
 // middleware that is specific to this router
 /* router.use((req, res, next) => {
 		console.log('Time: ', Date.now())
@@ -10,12 +13,14 @@ const router = Router();
 })
 */
 
-router.post("/register", UserController.register);
-router.post("/login", UserController.login);
 router.post("/changePassword", UserController.changePassword);
 router.post("/logout", UserController.logout);
-router.get("/getPublicProfile", UserController.getPublicProfile);
-router.post("/editInfo", UserController.editInfo);
+// router.get('/getPublicProfile', UserController.getPublicProfile)
+// router.post("/editInfo", UserController.editInfo);
+router.put("/addFriend", UserController.addFriend);
+router.put("/acceptFriendRequest", UserController.acceptFriendRequest);
+router.get("/getFriendRequests", UserController.getFriendRequests);
+router.get("/getFriends", UserController.getFriends);
 
 router.all("/checkSession", (req, res) => {
 	res.json({ response: req.session });
