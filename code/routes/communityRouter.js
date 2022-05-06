97d633f908f9948b4bc40656aac584b4c298bf13
alikates <alikates@dnyon.com>
2022-05-06 17:56:46 +0200

    Fix communityRouter

--- code/routes/communityRouter.js
+++ code/routes/communityRouter.js
@@ -1,24 +1,26 @@
-import { Router } from 'express';
-import User from '../models/User';
-import AsyncGame from '../models/asyncGame';
-import CommunityController from '../controller/CommunityController';
-const communityRouter = Router()
+import { Router } from "express";
+import CommunityController from "../controller/CommunityController";
 import validSession from "../util/validSession";
 
-// session validation middleware
-accountRouter.use(validSession);
+const communityRouter = Router();
 
+// session validation middleware
+communityRouter.use(validSession);
 
 communityRouter.put("/addFriend", CommunityController.addFriend);
 communityRouter.delete("/removeFriend", CommunityController.removeFriend);
-communityRouter.put("/acceptFriendRequest", CommunityController.acceptFriendRequest);
-communityRouter.get("/getFriendRequests", CommunityController.getFriendRequests);
+communityRouter.put(
+	"/acceptFriendRequest",
+	CommunityController.acceptFriendRequest
+);
+communityRouter.get(
+	"/getFriendRequests",
+	CommunityController.getFriendRequests
+);
 communityRouter.get("/getFriends", CommunityController.getFriends);
 //communityRouter.get('/getAllChats', CommunityController.getAllChats)
 //communityRouter.get('/sendMessage', CommunityController.sendMessage)
 //TODO añadir estos metodos
-communityRouter.get('/getPublicProfile', CommunityController.getPublicProfile)
+communityRouter.get("/getPublicProfile", CommunityController.getPublicProfile);
 
-
-
-export default communityRouter
+export default communityRouter;
