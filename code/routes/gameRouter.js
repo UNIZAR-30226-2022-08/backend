c70fbf20fbefaab1fc5988764c846ab7aec177e8
alikates <alikates@dnyon.com>
2022-05-17 14:11:01 +0200

    Add matchmaking functions and rename validSession to util

--- code/routes/gameRouter.js
+++ code/routes/gameRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import GameController from "../controller/GameController";
-import validSession from "../util/validSession";
+import { validSession } from "../util/util";
 
 const gameRouter = Router();
 
