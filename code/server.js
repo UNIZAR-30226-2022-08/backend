74feb20cecd6956bbb318426ada861a6924e408b
alikates <alikates@dnyon.com>
2022-05-20 17:24:42 +0200

    Initial websockets config

--- code/server.js
+++ code/server.js
@@ -9,6 +9,7 @@ import "dotenv/config";
 
 import database from "./database/database";
 import mainRouter from "./routes/mainRouter";
+import QueueWS from "./websockets/wss";
 
 const app = express();
 app.use(json());
@@ -25,15 +26,15 @@ app.use(
 	})
 );
 const oneDay = 1000 * 60 * 60 * 24;
-app.use(
-	sessions({
-		SameSite: 'none',
-		secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-		saveUninitialized: true,
-		cookie: { maxAge: oneDay },
-		resave: false,
-	})
-);
+
+const sessionParser = sessions({
+	SameSite: "none",
+	secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+	saveUninitialized: true,
+	cookie: { maxAge: oneDay },
+	resave: false,
+});
+app.use(sessionParser);
 app.use(cookieParser());
 
 app.use("/", mainRouter);
@@ -41,4 +42,16 @@ app.use("/", mainRouter);
 // Port Number
 const PORT = process.env.PORT || 5000;
 // Server Setup
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+server.on("upgrade", async (request, socket, head) => {
+	sessionParser(request, {}, () => {
+		if (request.session.username === undefined) {
+			socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+			socket.destroy();
+		}
+		QueueWS.handleUpgrade(request, socket, head, function done(ws) {
+			QueueWS.emit("connection", ws, request);
+		});
+	});
+});
