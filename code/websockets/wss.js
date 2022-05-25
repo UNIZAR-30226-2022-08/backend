57e23127a87bee5742880d29b005dbeae4aeeb10
alikates <alikates@dnyon.com>
2022-05-25 13:26:41 +0200

    Preparación websockets

--- code/websockets/wss.js
+++ code/websockets/wss.js
@@ -4,4 +4,24 @@ const QueueWS = new WebSocket.Server({
 	noServer: true /* , path: "/waitQueue" */,
 });
 
+QueueWS.on("connection", (ws, req) => {
+	const { username } = req.session;
+
+	ws.on("message", (body) => {
+		try {
+			const { to, message } = JSON.parse(body);
+			console.log(username);
+			console.log(to);
+			console.log(message);
+		} catch (error) {
+			console.trace();
+			console.error(error);
+		}
+	});
+	ws.on("syncGame", (body) => {
+		console.log(username);
+		// leer elo desde la bbdd
+	});
+});
+
 export default QueueWS;
